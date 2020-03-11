0bac63b0f43d7ffdb5d235dc6d7603c42b818da9
Daniel <demartinezgatell@gmail.com>
2020-03-11 17:28:32 +0000

    first component created

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Contact from "./components/Contact";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>The App Component</h1>
+      <Contact />
     </div>
   );
 }
---
+++ contactmanager/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Ulises Lima</h4>
+        <ul>
+          <li>Email: ulisesLima@gmail.com</li>
+          <li>Phone: 071142291029</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
