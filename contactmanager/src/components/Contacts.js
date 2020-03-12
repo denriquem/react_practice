6d9342570977444505cd29c62ed5706e77fff93d
Daniel <demartinezgatell@gmail.com>
2020-03-12 16:04:14 +0000

    adjusting state for contacts

---
+++ contactmanager/src/components/Contacts.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Ulises Lima",
+        email: "uliseslima@gmail.com",
+        phone: "9999999999"
+      },
+      {
+        id: 2,
+        name: "Karen Schmandelson",
+        email: "karenthecaptain@gmail.com",
+        phone: "01223 99999999"
+      },
+      {
+        id: 3,
+        name: "Richard Rickstein",
+        email: "richyboy@gmail.com",
+        phone: "07812 948 318"
+      }
+    ]
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Contacts;
