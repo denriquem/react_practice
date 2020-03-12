0f44feaae458cf0c78c83d8e3d9de5c3da821508
Daniel <demartinezgatell@gmail.com>
2020-03-12 13:28:26 +0000

    style: home page

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>The App Component</h1>
-      <Contact />
+      <Header branding="Contact Manager" />
+      <div className="container">
+        <Contact
+          name="Ulises Lima"
+          email="uliseslima@gmail.com"
+          phone="3333333333"
+        />
+        <Contact
+          name="Karen Schmandelson"
+          email="Karythecaptain@gmail.com"
+          phone="1111111111"
+        />
+      </div>
     </div>
   );
 }
