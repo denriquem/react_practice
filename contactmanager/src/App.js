b881a4fce30e78e375910e5ac48abbb7f7653868
Daniel Martinez-Gatell <danielmartinez-gatell@Daniels-MacBook-Pro.local>
2020-03-20 18:19:51 +0000

    feat: 404 page for incorrect routes

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 import { Provider } from "./context";
 
@@ -21,6 +22,7 @@ function App() {
               <Route exact path="/" component={Contacts} />
               <Route exact path="/contact/add" component={AddContact} />
               <Route exact path="/about/:id" component={About} />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </div>
